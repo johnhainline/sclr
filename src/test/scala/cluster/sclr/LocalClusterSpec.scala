cc32939de4c993f8c36044b2376e067cb0614091
John Hainline <john.hainline@gmail.com>
2018-01-19 14:59:03 -0600

    Beginning to experiment with fixing a local test.

--- src/test/scala/cluster/sclr/LocalClusterSpec.scala
+++ src/test/scala/cluster/sclr/LocalClusterSpec.scala
@@ -5,10 +5,9 @@
 //import akka.cluster.pubsub.{DistributedPubSub, DistributedPubSubMediator}
 //import akka.pattern.gracefulStop
 //import akka.testkit.{ImplicitSender, TestKit}
-//import cluster.sclr.Messages.{Begin, BeginAck, Finished, Ready}
+//import cluster.sclr.Messages.{Ack, Finished, Workload}
 //import cluster.sclr.actors.{ComputeActor, ManageActor}
 //import cluster.sclr.doobie.ResultsDao
-//import combinations.{CombinationAggregation, CombinationBuilder}
 //import org.scalamock.scalatest.MockFactory
 //import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 //
@@ -34,15 +33,17 @@
 //
 //    "process work using a single compute node" in {
 //      val resultsDao = mock[ResultsDao]
-//      (resultsDao.insertResult _).expects("Vector(Vector(0), Vector(0, 1))")
-//      val combinations = new CombinationAggregation(Vector(new CombinationBuilder(1,1), new CombinationBuilder(2,2)))
-//      val manageActor  = system.actorOf(ManageActor.props(), "manage")
+//      (resultsDao.setupDatabase _).expects().returning(1)
+//      (resultsDao.setupTable _).expects("test", 1, 1, 2).returning(1)
+//      (resultsDao.insertResult _).expects("test", true, Vector(0), Vector(0), Vector(0.0,1.0))
+//      val manageActor  = system.actorOf(ManageActor.props(resultsDao), "manage")
 //      val computeActor = system.actorOf(ComputeActor.props(resultsDao), "compute")
+//      val workload = Workload("test", 1,1,1,1)
 //
-//      manageActor ! Begin(combinations)
-//      expectMsg(1 seconds, BeginAck)
+//      manageActor ! workload
+//      expectMsg(1 seconds, Ack)
 //
-//      expectMsg(1 seconds, Ready)
+//      expectMsg(1 seconds, workload)
 //      expectMsg(5 seconds, Finished)
 //      Await.result(gracefulStop(computeActor, 5 seconds), Duration.Inf)
 //      Await.result(gracefulStop(manageActor, 5 seconds), Duration.Inf)
@@ -50,23 +51,23 @@
 //
 //    "process work using 3 compute nodes" in {
 //      val resultsDao = mock[ResultsDao]
-//      (resultsDao.insertResult _).expects("Vector(Vector(0, 1, 2), Vector(0))")
-//      (resultsDao.insertResult _).expects("Vector(Vector(0, 1, 2), Vector(1))")
-//      (resultsDao.insertResult _).expects("Vector(Vector(0, 1, 3), Vector(0))")
-//      (resultsDao.insertResult _).expects("Vector(Vector(0, 1, 3), Vector(1))")
-//      (resultsDao.insertResult _).expects("Vector(Vector(0, 2, 3), Vector(0))")
-//      (resultsDao.insertResult _).expects("Vector(Vector(0, 2, 3), Vector(1))")
-//      (resultsDao.insertResult _).expects("Vector(Vector(1, 2, 3), Vector(0))")
-//      (resultsDao.insertResult _).expects("Vector(Vector(1, 2, 3), Vector(1))")
+////      (resultsDao.insertResult _).expects("Vector(Vector(0, 1, 2), Vector(0))")
+////      (resultsDao.insertResult _).expects("Vector(Vector(0, 1, 2), Vector(1))")
+////      (resultsDao.insertResult _).expects("Vector(Vector(0, 1, 3), Vector(0))")
+////      (resultsDao.insertResult _).expects("Vector(Vector(0, 1, 3), Vector(1))")
+////      (resultsDao.insertResult _).expects("Vector(Vector(0, 2, 3), Vector(0))")
+////      (resultsDao.insertResult _).expects("Vector(Vector(0, 2, 3), Vector(1))")
+////      (resultsDao.insertResult _).expects("Vector(Vector(1, 2, 3), Vector(0))")
+////      (resultsDao.insertResult _).expects("Vector(Vector(1, 2, 3), Vector(1))")
 //
-//      val combinations = new CombinationAggregation(Vector(new CombinationBuilder(4,3), new CombinationBuilder(2,1)))
-//      val manageActor = system.actorOf(ManageActor.props(), "manage")
+//      val manageActor = system.actorOf(ManageActor.props(resultsDao), "manage")
 //      val computeActors = (for (i <- 1 to 3) yield system.actorOf(ComputeActor.props(resultsDao))).toVector
+//      val workload = Workload("test", 4, 3, 2, 1)
 //
-//      manageActor ! Begin(combinations)
-//      expectMsg(1 seconds, BeginAck)
+//      manageActor ! workload
+//      expectMsg(1 seconds, Ack)
 //
-//      expectMsg(1 seconds, Ready)
+//      expectMsg(1 seconds, workload)
 //      expectMsg(5 seconds, Finished)
 //      for (actor <- computeActors) {
 //        Await.result(gracefulStop(actor, 5 seconds), Duration.Inf)
