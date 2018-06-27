b35da10af020ab833bef6dc6228d73adfdfef243
= <john.hainline@gmail.com>
2018-06-27 14:45:05 -0500

    Added some (kinda crappy) logic for shutting down the ManageActor when its source iterator completes.

--- src/main/scala/sclr/core/actors/ManageActor.scala
+++ src/main/scala/sclr/core/actors/ManageActor.scala
@@ -1,22 +1,21 @@
 package sclr.core.actors
 
-import akka.{Done, NotUsed}
+import akka.Done
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import akka.cluster.pubsub.DistributedPubSub
 import akka.cluster.pubsub.DistributedPubSubMediator.Publish
 import akka.remote.Ack
-import akka.stream.contrib.Retry
-import akka.stream.{ActorMaterializer, Attributes, ClosedShape, KillSwitches}
 import akka.stream.scaladsl._
+import akka.stream.{ActorMaterializer, Attributes, KillSwitches}
 import cats.effect.IO
-import sclr.core.Messages._
-import sclr.core.database.{DatabaseDao, DatasetInfo, Result}
-import sclr.core.http.SclrService
 import combinations.Combinations
 import combinations.iterators.MultipliedIterator
 import doobie.util.transactor.Transactor
+import sclr.core.Messages._
 import sclr.core.actors.LifecycleActor.{ManageStreamCompleted, ManageStreamFailed, ManageStreamStarted}
 import sclr.core.actors.ManageActor.DB_CONNECTIONS
+import sclr.core.database.{DatabaseDao, DatasetInfo, Result}
+import sclr.core.http.SclrService
 import streams.BalanceHub
 
 import scala.concurrent.duration._
@@ -83,25 +82,39 @@ class ManageActor(lifecycleActor: ActorRef, infoService: SclrService, dao: Datab
     val iteratorGen = () => ManageActor.createIterator(rowCount = info.rowCount, yLength = info.yLength, rowsConstant = workload.getRowsConstant(), workload.optionalSubset, r)
 
     // Materializing these objects lets us eventually generate SourceRef/SinkRef instances to bind to ComputeActor.
-    val balanceHub = BalanceHub.sink[Work](bufferSize = 16)
-    val mergeHub = MergeHub.source[Result](perProducerBufferSize = 1)
+    val balanceHub = BalanceHub.sink[Work](bufferSize = 32)
+    val mergeHub = MergeHub.source[Result](perProducerBufferSize = 16)
     val hubFlow = Flow.fromSinkAndSourceMat(balanceHub, mergeHub)(Keep.both)
 
     val source  = Source.fromIterator(iteratorGen)
+    val watch   = Flow[Work].watchTermination()(Keep.right)
     val groupRs = Flow[Result].groupedWithin(50, 100 milliseconds)
     val save    = Sink.foreachParallel(parallelism = DB_CONNECTIONS) {dao.insertResults(xa, workload.name)}
                     .addAttributes(Attributes.inputBuffer(initial=4, max=100))
 
     // Materialize the stream, getting back our endlessly materializable source and sink from BalanceHub and MergeHub.
-    val (((pullWorkSource, pushResultsSink), killSwitch), futureDone) = source
-      .viaMat(hubFlow)(Keep.right)
+    val (((sourceDone, (pullWorkSource, pushResultsSink)), killSwitch), sinkDone) =
+      source
+      .viaMat(watch)(Keep.right)
+      .viaMat(hubFlow)(Keep.both)
       .viaMat(KillSwitches.single)(Keep.both)
       .viaMat(groupRs)(Keep.left)
       .toMat(save)(Keep.both)
       .run()
 
+    // When the source is complete, stop the flow AFTER the MergeHub. This is necessary because MergeHub never stops
+    // unless it gets a "cancel" from downstream. We wait 10 seconds to allow the system to drain (which sucks).
+    sourceDone.onComplete {
+      case Success(Done) =>
+        context.system.scheduler.scheduleOnce(delay = 10 seconds, new Runnable {
+          override def run(): Unit = killSwitch.shutdown()
+        })
+      case Failure(ex) =>
+        killSwitch.abort(ex)
+    }
+
     // When we terminate, send a message to the LifecycleActor.
-    futureDone.onComplete {
+    sinkDone.onComplete {
       case Success(Done) =>
         lifecycleActor ! ManageStreamCompleted(self)
       case Failure(ex) =>
@@ -121,94 +134,94 @@ class ManageActor(lifecycleActor: ActorRef, infoService: SclrService, dao: Datab
     makeConnection
   }
 
-  private def makeConnectionFunctionOuterRetry(xa: Transactor[IO], dao: DatabaseDao, info: DatasetInfo, workload: Workload, r: Random): WorkComputeReady => Unit = {
-    def partitionFunction(tuple: (Try[Result], Work)): Int = {
-      tuple match {
-        case (Success(_), _) => 0
-        case (Failure(_), _) => 1
-        case _ => throw new RuntimeException(s"Received invalid entry $tuple")
-      }
-    }
-
-    // An iterator that runs through (ySize choose 2) * (rows choose 2)
-    val iteratorGen = () => ManageActor.createIterator(rowCount = info.rowCount, yLength = info.yLength, rowsConstant = workload.getRowsConstant(), workload.optionalSubset, r)
-
-    // Materializing these objects lets us eventually generate SourceRef/SinkRef instances to bind to ComputeActor.
-    val hubFlow = Flow.fromSinkAndSourceCoupledMat(BalanceHub.sink[Work](), MergeHub.source[Result])(Keep.both)
-      .map[Try[Result]](Success(_))//.recover{ case e: Exception => Failure(e)}
-
-    val entireStream = RunnableGraph.fromGraph(GraphDSL.create(hubFlow) { implicit b => implicit hub =>
-      import GraphDSL.Implicits._
-
-      val source  = Source.fromIterator(iteratorGen)
-      val merge   = b.add(MergePreferred[Work](secondaryPorts = 1, eagerComplete = false))
-      val bcast   = b.add(Broadcast[Work](outputPorts = 2, eagerCancel = false))
-      val zip     = b.add(Zip[Try[Result], Work]())
-      val part    = b.add(Partition[(Try[Result], Work)](outputPorts = 2, partitioner = partitionFunction))
-      val getR    = b.add(Flow[(Try[Result], Work)].map(_._1.get))
-      val getW    = b.add(Flow[(Try[Result], Work)].map(_._2))
-      val groupRs = b.add(Flow[Result].groupedWithin(50, 100 milliseconds))
-      val save    = Sink.foreachParallel(parallelism = DB_CONNECTIONS) {dao.insertResults(xa, workload.name)}
-
-      source ~> merge.in(0)
-      merge.out ~> bcast ~> hub ~> zip.in0
-      bcast        ~> zip.in1
-      zip.out ~> part.in
-      part.out(0) ~> getR ~> groupRs ~> save
-      part.out(1) ~> getW ~> merge.preferred
-      ClosedShape
-    })
-
-    // Materialize the stream, getting back our endlessly materializable source and sink from BalanceHub and MergeHub.
-    val (pullWorkSource, pushResultsSink) = entireStream.run()
-    val makeConnection: WorkComputeReady => Unit = (workComputeReady: WorkComputeReady) => {
-      val computeFlow = Flow.fromSinkAndSourceCoupled(workComputeReady.pullWork, workComputeReady.pushResult)
-      pullWorkSource.via(computeFlow).to(pushResultsSink).run()
-    }
-    makeConnection
-  }
-
-  private def makeConnectionFunctionInnerRetry(xa: Transactor[IO], dao: DatabaseDao, info: DatasetInfo, workload: Workload, r: Random): WorkComputeReady => Unit = {
-    // An iterator that runs through (ySize choose 2) * (rows choose 2)
-    val iteratorGen = () => ManageActor.createIterator(rowCount = info.rowCount, yLength = info.yLength, rowsConstant = workload.getRowsConstant(), workload.optionalSubset, r)
-
-    val workSource = Source.fromIterator(iteratorGen)
-    val saveResultsSink = Sink.foreachParallel(parallelism = DB_CONNECTIONS) {
-      dao.insertResults(xa, workload.name)
-    }
-
-    val hubFlow = Flow.fromSinkAndSourceCoupledMat(BalanceHub.sink[(Work, Work)](), MergeHub.source[(Try[Result], Work)])(Keep.both)
-
-    // Always retry failed work.
-    val retryFlow = Retry(hubFlow) { work => Some((work, work)) }
-
-    // Construct workSource -> Work = (Work, Work) -> Retry(BalanceHub sink -> MergeHub source) -> (Try[Result], Work) = Result -> chunk Results -> save Results
-    val expand = Flow[Work].map(work => (work, work))
-    val flatten = Flow[(Try[Result], Work)].map(tuple => tuple._1.get)
-    val entireStream = workSource.via(expand).viaMat(retryFlow)(Keep.right).via(flatten).groupedWithin(50, 100 milliseconds).to(saveResultsSink)
-
-    // Materialize the stream, getting back our endlessly materializable source and sink from BalanceHub and MergeHub.
-    val (pullWorkSource, pushResultsSink) = entireStream.run()
-
-    // Construct an anonymous method that creates our connection to a WorkComputeReady instance.
-    val makeConnection: WorkComputeReady => Unit = (workComputeReady: WorkComputeReady) => {
-      val computeFlow = Flow.fromSinkAndSourceCoupled(workComputeReady.pullWork, workComputeReady.pushResult)
-        .map[Try[Result]](Success(_)).recover { case e: Exception => Failure(e) }
-
-      val stream = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
-        import GraphDSL.Implicits._
-        val in = builder.add(Flow[(Work, Work)].map(_._1)) // (Work, Work) => Work
-      val broadcast = builder.add(Broadcast[Work](outputPorts = 2, eagerCancel = true))
-        val zip = builder.add(Zip[Try[Result], Work]())
-        pullWorkSource ~> in ~> broadcast ~> computeFlow ~> zip.in0
-        broadcast ~> zip.in1
-        zip.out ~> pushResultsSink
-        ClosedShape
-      })
-      stream.run()
-    }
-    makeConnection
-  }
+//  private def makeConnectionFunctionOuterRetry(xa: Transactor[IO], dao: DatabaseDao, info: DatasetInfo, workload: Workload, r: Random): WorkComputeReady => Unit = {
+//    def partitionFunction(tuple: (Try[Result], Work)): Int = {
+//      tuple match {
+//        case (Success(_), _) => 0
+//        case (Failure(_), _) => 1
+//        case _ => throw new RuntimeException(s"Received invalid entry $tuple")
+//      }
+//    }
+//
+//    // An iterator that runs through (ySize choose 2) * (rows choose 2)
+//    val iteratorGen = () => ManageActor.createIterator(rowCount = info.rowCount, yLength = info.yLength, rowsConstant = workload.getRowsConstant(), workload.optionalSubset, r)
+//
+//    // Materializing these objects lets us eventually generate SourceRef/SinkRef instances to bind to ComputeActor.
+//    val hubFlow = Flow.fromSinkAndSourceCoupledMat(BalanceHub.sink[Work](), MergeHub.source[Result])(Keep.both)
+//      .map[Try[Result]](Success(_))//.recover{ case e: Exception => Failure(e)}
+//
+//    val entireStream = RunnableGraph.fromGraph(GraphDSL.create(hubFlow) { implicit b => implicit hub =>
+//      import GraphDSL.Implicits._
+//
+//      val source  = Source.fromIterator(iteratorGen)
+//      val merge   = b.add(MergePreferred[Work](secondaryPorts = 1, eagerComplete = false))
+//      val bcast   = b.add(Broadcast[Work](outputPorts = 2, eagerCancel = false))
+//      val zip     = b.add(Zip[Try[Result], Work]())
+//      val part    = b.add(Partition[(Try[Result], Work)](outputPorts = 2, partitioner = partitionFunction))
+//      val getR    = b.add(Flow[(Try[Result], Work)].map(_._1.get))
+//      val getW    = b.add(Flow[(Try[Result], Work)].map(_._2))
+//      val groupRs = b.add(Flow[Result].groupedWithin(50, 100 milliseconds))
+//      val save    = Sink.foreachParallel(parallelism = DB_CONNECTIONS) {dao.insertResults(xa, workload.name)}
+//
+//      source ~> merge.in(0)
+//      merge.out ~> bcast ~> hub ~> zip.in0
+//      bcast        ~> zip.in1
+//      zip.out ~> part.in
+//      part.out(0) ~> getR ~> groupRs ~> save
+//      part.out(1) ~> getW ~> merge.preferred
+//      ClosedShape
+//    })
+//
+//    // Materialize the stream, getting back our endlessly materializable source and sink from BalanceHub and MergeHub.
+//    val (pullWorkSource, pushResultsSink) = entireStream.run()
+//    val makeConnection: WorkComputeReady => Unit = (workComputeReady: WorkComputeReady) => {
+//      val computeFlow = Flow.fromSinkAndSourceCoupled(workComputeReady.pullWork, workComputeReady.pushResult)
+//      pullWorkSource.via(computeFlow).to(pushResultsSink).run()
+//    }
+//    makeConnection
+//  }
+//
+//  private def makeConnectionFunctionInnerRetry(xa: Transactor[IO], dao: DatabaseDao, info: DatasetInfo, workload: Workload, r: Random): WorkComputeReady => Unit = {
+//    // An iterator that runs through (ySize choose 2) * (rows choose 2)
+//    val iteratorGen = () => ManageActor.createIterator(rowCount = info.rowCount, yLength = info.yLength, rowsConstant = workload.getRowsConstant(), workload.optionalSubset, r)
+//
+//    val workSource = Source.fromIterator(iteratorGen)
+//    val saveResultsSink = Sink.foreachParallel(parallelism = DB_CONNECTIONS) {
+//      dao.insertResults(xa, workload.name)
+//    }
+//
+//    val hubFlow = Flow.fromSinkAndSourceCoupledMat(BalanceHub.sink[(Work, Work)](), MergeHub.source[(Try[Result], Work)])(Keep.both)
+//
+//    // Always retry failed work.
+//    val retryFlow = Retry(hubFlow) { work => Some((work, work)) }
+//
+//    // Construct workSource -> Work = (Work, Work) -> Retry(BalanceHub sink -> MergeHub source) -> (Try[Result], Work) = Result -> chunk Results -> save Results
+//    val expand = Flow[Work].map(work => (work, work))
+//    val flatten = Flow[(Try[Result], Work)].map(tuple => tuple._1.get)
+//    val entireStream = workSource.via(expand).viaMat(retryFlow)(Keep.right).via(flatten).groupedWithin(50, 100 milliseconds).to(saveResultsSink)
+//
+//    // Materialize the stream, getting back our endlessly materializable source and sink from BalanceHub and MergeHub.
+//    val (pullWorkSource, pushResultsSink) = entireStream.run()
+//
+//    // Construct an anonymous method that creates our connection to a WorkComputeReady instance.
+//    val makeConnection: WorkComputeReady => Unit = (workComputeReady: WorkComputeReady) => {
+//      val computeFlow = Flow.fromSinkAndSourceCoupled(workComputeReady.pullWork, workComputeReady.pushResult)
+//        .map[Try[Result]](Success(_)).recover { case e: Exception => Failure(e) }
+//
+//      val stream = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
+//        import GraphDSL.Implicits._
+//        val in = builder.add(Flow[(Work, Work)].map(_._1)) // (Work, Work) => Work
+//      val broadcast = builder.add(Broadcast[Work](outputPorts = 2, eagerCancel = true))
+//        val zip = builder.add(Zip[Try[Result], Work]())
+//        pullWorkSource ~> in ~> broadcast ~> computeFlow ~> zip.in0
+//        broadcast ~> zip.in1
+//        zip.out ~> pushResultsSink
+//        ClosedShape
+//      })
+//      stream.run()
+//    }
+//    makeConnection
+//  }
 }
 
 object ManageActor {
