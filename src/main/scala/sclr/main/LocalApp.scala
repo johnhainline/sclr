0e9cd8c0f220f003942db86c57579f86528e0d1a
= <john.hainline@gmail.com>
2018-07-28 13:27:40 -0500

    Adding dataset.

--- src/main/scala/sclr/main/LocalApp.scala
+++ src/main/scala/sclr/main/LocalApp.scala
@@ -3,10 +3,13 @@ package sclr.main
 object LocalApp {
   def main(args: Array[String]): Unit = {
 //    val json = """{"name":"tiny","dnfSize":2,"optionalSubset":10,"useLPNorm":true,"mu":0.2, "optionalRandomSeed":1}"""
-    val json = """{"name":"rsv","dnfSize":2,"useLPNorm":true,"mu":0.2}"""
+    val json = """{"name":"boston_train2","dnfSize":2,"useLPNorm":true,"mu":0.2}"""
     Sclr.run(Array[String]("-w", json, "-p", "4"))
 
-//    val sleep = 3000
+//    Run with different sampling values from 150 to 50 (step = -10).
+//    Mu varies from 0.2 to 0.6 (step = 0.2).
+
+    //    val sleep = 3000
 //    Thread.sleep(sleep)
 //    system.actorSelection("/user/computeSupervisor") ! Kill
 //    Thread.sleep(sleep)
